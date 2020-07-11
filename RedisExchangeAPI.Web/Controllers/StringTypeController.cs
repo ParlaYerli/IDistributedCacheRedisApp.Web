2b851d943761e0445e3406e57049fa1ac221a5f0
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2020-07-11 22:08:51 +0300

    new project

---
+++ RedisExchangeAPI.Web/Controllers/StringTypeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Servers;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class StringTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase database;
+
+       
+        public StringTypeController(RedisService redisService)
+        {
+           _redisService = redisService;
+           database = _redisService.GetDB(1);
+        }
+        public IActionResult Index()
+        {
+
+            database.StringSet("name", "Parla");
+            database.StringSet("ziyaretci", 100);
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            var value = database.StringGet("name");
+
+            if (value.HasValue)
+            {
+                ViewBag.value = value.ToString();
+            }
+            return View();
+        }
+    }
+}
---
+++ RedisExchangeAPI.Web/Servers/RedisService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Servers
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+        private IConnectionMultiplexer _redis; //redis server ile haberleşmeyi sağlar.
+        public IDatabase db { get; set; }
+        public RedisService(IConfiguration _configuration)
+        {
+            _redisHost = _configuration["Redis:Host"];
+            _redisPort = _configuration["Redis:Port"];
+        }
+
+        // proje ayağa kalktığında redis server ile iletişime geçecek.
+        public void Connect()
+        {
+            var configString = $"{_redisHost}:{_redisPort}";
+            _redis = ConnectionMultiplexer.Connect(configString);
+        }
+
+        public IDatabase GetDB(int db) //  Redis içerisinde hazır sıralı dbler mevcut. Bu databaselerden istediğimi seçmek için bu metodu kullanacağım.
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
